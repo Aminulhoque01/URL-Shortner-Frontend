e33f0afd4e904164354624f51e7e84312229012b
Aminulhoque01 <aminuhoque53@gmail.com>
2026-01-07 10:39:41 +0600

    auth api

---
+++ src/features/auth/authApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+  }),
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/api/auth/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/api/auth/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
